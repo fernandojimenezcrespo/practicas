1dbc6ec1ceb7939491657f2f152e1e4ffc148082
fernandojimenezcrespo <fernandojimenezcrespo@gmail.com>
2024-02-19 08:25:56 +0100

    primer commit

---
+++ src/main/java/org/example/MainView.java
@@ -0,0 +1,162 @@
+package org.example;
+
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.combobox.ComboBox;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.component.datepicker.DatePicker;
+import com.vaadin.flow.component.formlayout.FormLayout;
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.grid.GridVariant;
+import com.vaadin.flow.data.binder.Binder;
+import com.vaadin.flow.data.binder.ValidationException;
+import com.vaadin.flow.router.Route;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@Route
+public class MainView extends HorizontalLayout {
+
+    /* definimos aquí los campos para poder usarlos en cualquier orden.
+    Si los definimos dentro de MainView habría que definir button, por ejempo, 
+    antes de poder usarlo en el evento click
+     */
+    private Button nuevo = new Button("Nuevo");
+    private Button ok = new Button("Aceptar");
+    private Button cancel = new Button("Cancelar");
+    private TextField dni = new TextField("NIF");
+    private TextField nombre = new TextField("Nombre");
+    private TextField ape1 = new TextField("Apellido 1");
+    private TextField ape2 = new TextField("Apellido 2");
+    private TextField calle = new TextField("Calle");
+    private TextField numero = new TextField("Número");
+    private TextField piso = new TextField("Piso/Letra");
+    private TextField cp = new TextField("Código Postal");
+    private DatePicker fechaNac = new DatePicker("Fecha de nacimiento");
+    private ComboBox<String> tipoVia = new ComboBox("Tipo de vía");
+    private Grid<UsuarioBean> userGrid = new Grid<>(UsuarioBean.class, false);
+    private Binder<UsuarioBean> binder = new Binder();
+    private Binder<Direccion> binderD = new Binder();
+    private HashSet<UsuarioBean> lista = new UsuarioDao().getLista();
+
+    /* Este método debería leer de la base de datos; 
+para trabajar con esto hemos definido valores en el código (en UsuarioDao.java)
+     */
+
+    public MainView() {
+
+        ok.setEnabled(false);
+        /* por defecto desactivado, hasta que haya algo en los campos */
+
+        tipoVia.setAllowCustomValue(true);
+        tipoVia.setItems("Calle", "Paseo", "Plaza", "Ctra.", "Parque");
+
+        binder.forField(dni).asRequired("El NIF es oblgatorio").bind(UsuarioBean::getDni, UsuarioBean::setDni);
+        binder.forField(nombre).asRequired("Es obligatorio el  nombre").bind(UsuarioBean::getNombre, UsuarioBean::setNombre);
+        
+        binder.forField(ape1).bind(UsuarioBean::getApe1, UsuarioBean::setApe1);
+        binder.forField(ape2).bind(UsuarioBean::getApe2, UsuarioBean::setApe2);
+        binder.forField(fechaNac).bind(UsuarioBean::getFechaNacimiento, UsuarioBean::setFechaNacimiento);
+
+        binderD.forField(tipoVia).bind(Direccion::getTipovia, Direccion::setTipovia);
+        binderD.forField(calle).bind(Direccion::getDireccion, Direccion::setDireccion);
+        binderD.forField(numero).bind(Direccion::getNumero, Direccion::setNumero);
+        binderD.forField(piso).bind(Direccion::getPiso, Direccion::setPiso);
+        binderD.forField(cp).bind(Direccion::getCodigoPostal, Direccion::setCodigoPostal);
+
+        /*click en nuevo usuario*/
+        nuevo.addClickListener(e -> {
+
+            try {
+                Direccion dir = new Direccion();
+                binderD.writeBean(dir);
+
+                UsuarioBean usu = new UsuarioBean();
+                binder.writeBean(usu);
+
+                usu.setDireccion(dir);
+                lista.add(usu);
+                userGrid.setItems(lista);
+                
+                dni.clear();
+                nombre.clear();
+                ape1.clear();
+                ape2.clear();
+                calle.clear();
+                numero.clear();
+                piso.clear();
+                cp.clear();
+                ape1.clear();
+                fechaNac.clear();
+                tipoVia.clear();
+              
+                } catch (ValidationException ex) {
+                Logger.getLogger(MainView.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        });
+
+        userGrid.addItemClickListener(e -> {
+            
+            binder.readBean(e.getItem());
+            /* rellena los campos en pantalla*/
+            binderD.readBean(e.getItem().getDireccion());
+            ok.setEnabled(true);
+        });
+        userGrid.addThemeVariants(GridVariant.LUMO_COLUMN_BORDERS);
+        userGrid.addColumn(UsuarioBean::getDni).setHeader("NIF")
+                .setSortable(true)
+                .setResizable(true);
+        userGrid.addColumn(UsuarioBean::getNombre).setHeader("Nombre").setSortable(true)
+                .setResizable(true);
+        userGrid.addColumn(UsuarioBean::getApe1).setHeader("Apellido 1").setSortable(true)
+                .setResizable(true);
+        userGrid.addColumn(UsuarioBean::getApe2).setHeader("Apellido 2").setSortable(true)
+                .setResizable(true);
+        userGrid.addColumn(UsuarioBean::getFechaNacimiento).setHeader("Fecha de nacimiento").setSortable(true)
+                .setResizable(true);
+        userGrid.addColumn(UsuarioBean::getDireccion).setHeader("Dirección")
+                .setResizable(true);
+        userGrid.setItems(lista);
+
+        VerticalLayout verticalGLOBAL = new VerticalLayout();
+        HorizontalLayout horizontalUsuario = new HorizontalLayout();
+        HorizontalLayout horizontalDomicilio = new HorizontalLayout();
+        HorizontalLayout horizontalBotones =new HorizontalLayout();
+        
+
+        verticalGLOBAL.add("Usuario");
+
+        add(verticalGLOBAL);
+
+        dni.setClearButtonVisible(true);
+        dni.setMinLength(9);
+        dni.setMaxLength(9);
+
+        horizontalUsuario.add(dni);
+        horizontalUsuario.add(nombre);
+        horizontalUsuario.add(ape1);
+        horizontalUsuario.add(ape2);
+        horizontalUsuario.add(fechaNac);
+        horizontalDomicilio.add(tipoVia);
+        horizontalDomicilio.add(calle);
+        horizontalDomicilio.add(numero);
+        horizontalDomicilio.add(piso);
+        horizontalDomicilio.add(cp);
+        
+        horizontalBotones.add(nuevo);
+        horizontalBotones.add(ok);
+        horizontalBotones.add(cancel);
+        
+        verticalGLOBAL.add(horizontalUsuario);
+        verticalGLOBAL.add(horizontalDomicilio);
+        verticalGLOBAL.add(horizontalBotones);
+        verticalGLOBAL.add(userGrid);
+
+        
+
+    }
+
+}
---
+++ src/main/java/org/example/UsuarioDao.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.example;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+
+/**
+ *
+ * @author 06544740K
+ */
+public class UsuarioDao {
+    
+    
+    public HashSet<UsuarioBean> getLista (){
+        HashSet<UsuarioBean> lista = new HashSet<>();
+        
+        LocalDate fecha = LocalDate.of(2000, 12, 8);
+        Direccion direccion = new Direccion ("Calle", "Arce","1","3º","05001");
+        UsuarioBean usuario = new UsuarioBean ("11122333Q", "Manolo","ape1", "ape2",fecha, direccion);
+        lista.add(usuario);
+        
+        fecha = LocalDate.of(1980, 3, 1);
+        direccion = new Direccion ("Calle", "Pino","2","1º","05001");
+        usuario = new UsuarioBean ("66699966Z", "Pistón","ape1", "ape22",fecha, direccion);
+        lista.add(usuario);
+        
+        return lista;
+        
+    };
+    
+    
+}
